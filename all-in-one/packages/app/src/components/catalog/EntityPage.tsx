dc9b175e7763e3f8a11a57079e67028155fcf7b4
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-05-17 08:21:44 +0200

    Add kubernetes frontend and backend

--- all-in-one/packages/app/src/components/catalog/EntityPage.tsx
+++ all-in-one/packages/app/src/components/catalog/EntityPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+EntityPage.tsximport React from 'react';
 import { Button, Grid } from '@material-ui/core';
 import {
   EntityApiDefinitionCard,
@@ -53,6 +53,8 @@ import {
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
+
+import { EntityKubernetesContent } from '@backstage/plugin-kubernetes';
 
 const techdocsContent = (
   <EntityTechdocsContent>
@@ -175,6 +177,10 @@ const serviceEntityPage = (
     <EntityLayout.Route path="/docs" title="Docs">
       {techdocsContent}
     </EntityLayout.Route>
+
+    <EntityLayout.Route path="/kubernetes" title="Kubernetes">
+      <EntityKubernetesContent refreshIntervalMs={30000} />
+    </EntityLayout.Route>
   </EntityLayout>
 );
 
@@ -201,6 +207,10 @@ const websiteEntityPage = (
 
     <EntityLayout.Route path="/docs" title="Docs">
       {techdocsContent}
+    </EntityLayout.Route>
+
+    <EntityLayout.Route path="/kubernetes" title="Kubernetes">
+      <EntityKubernetesContent refreshIntervalMs={30000} />
     </EntityLayout.Route>
   </EntityLayout>
 );
