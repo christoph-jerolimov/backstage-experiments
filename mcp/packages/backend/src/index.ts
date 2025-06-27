eef41e6d65edea34464e33d984a89d39f8ced2f1
Christoph Jerolimov <jerolimov+git@gmail.com>
2025-06-27 12:08:29 +0200

    mcp: tested catalog action

--- mcp/packages/backend/src/index.ts
+++ mcp/packages/backend/src/index.ts
@@ -52,4 +52,7 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
+// mcp
+backend.add(import('@backstage/plugin-mcp-actions-backend'));
+
 backend.start();
