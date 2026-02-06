11ed809a65cfa4a55b6d4f5dc9634e02f3ee301d
Christoph Jerolimov <jerolimov+git@gmail.com>
2026-02-06 15:54:27 +0100

    add npm plugin to play

--- nfs-customization/packages/app/src/App.tsx
+++ nfs-customization/packages/app/src/App.tsx
@@ -2,6 +2,8 @@ import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
 
+import npmPlugin from '@backstage-community/plugin-npm/alpha';
+
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [catalogPlugin, navModule, npmPlugin],
 });
